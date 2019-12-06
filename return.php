bff56fd6ed95036b4a4bcf22db00d0c15394ff6b
Andrii Sememets <semteacher@gmail.com>
2019-12-06 19:22:31 +0200

    change type of description db field to text small clean-up

--- return.php
+++ return.php
@@ -93,10 +93,7 @@ $data->payment_type     = $pdata->data['type'];              // payment type
 $data->err_code         = !empty($pdata->data['err_code'])? $pdata->data['err_code'] : ''; // Transaction error code
 $data->timeupdated      = time();
 
-var_dump($data);
-//$user = $DB->get_record("user", array("id" => $data->userid), "*", MUST_EXIST);
 $course = $DB->get_record("course", array("id" => $data->courseid), "*", MUST_EXIST);
-//$context = context_course::instance($course->id, MUST_EXIST);
 $context = context_course::instance($data->courseid, MUST_EXIST);
 $destination = "$CFG->wwwroot/course/view.php?id=$data->courseid";
 $fullname = format_string($course->fullname, true, array('context' => $context));
@@ -112,14 +109,14 @@ if ((strlen($pdata->data["action"]) > 0) && (strlen($pdata->data["status"]) > 0)
     if ((strcmp($pdata->data["action"], "pay") == 0) && (strcmp($pdata->data["status"], "success") == 0)) { // VALID PAYMENT!
 
         // Fill rest of transaction data - only if more or less success
-        $data->payment_id       = $pdata->data['payment_id'];        // ==transaction_id
+        $data->payment_id        = $pdata->data['payment_id'];        // ==transaction_id
         $data->description       = $pdata->data['description'];
-        $data->commission_credit = $pdata->data['commission_credit'];// commission from receiver
-        $data->amount_debit      = $pdata->data['amount_debit'];     // payed by customer
-        $data->currency_debit    = $pdata->data['currency_debit'];   // currency of customer's payment
-        $data->acq_id           = $pdata->data['acq_id'];            // An Equirer ID
-        $data->end_date         = $pdata->data['end_date'];          // Transaction end date
-        $data->create_date      = $pdata->data['create_date'];       // Transaction create date
+        $data->commission_credit = $pdata->data['commission_credit']; // commission from receiver
+        $data->amount_debit      = $pdata->data['amount_debit'];      // payed by customer
+        $data->currency_debit    = $pdata->data['currency_debit'];    // currency of customer's payment
+        $data->acq_id            = $pdata->data['acq_id'];            // An Equirer ID
+        $data->end_date          = $pdata->data['end_date'];          // Transaction end date
+        $data->create_date       = $pdata->data['create_date'];       // Transaction create date
         $data->paytype           = $pdata->data['paytype']; // card - оплата картой, liqpay - через кабинет liqpay,
                                                             // privat24 - через кабинет приват24, 
                                                             // masterpass - через кабинет masterpass, 
@@ -162,8 +159,6 @@ if ((strlen($pdata->data["action"]) > 0) && (strlen($pdata->data["status"]) > 0)
             redirect($CFG->wwwroot, get_string('nocourse', 'enrol_liqpay', $data->courseid));
         }
 
-        //$coursecontext = context_course::instance($course->id, IGNORE_MISSING);
-
         // Check that amount paid is the correct amount
         if ( (float) $plugin_instance->cost <= 0 ) {
             $cost = (float) $plugin->get_config('cost');
@@ -175,7 +170,9 @@ if ((strlen($pdata->data["action"]) > 0) && (strlen($pdata->data["status"]) > 0)
         $cost = format_float($cost, 2, false);
 
         if ($data->payment_gross < $cost) {
-            \enrol_liqpay\util::message_liqpay_error_to_admin("Amount paid is not enough ($data->payment_gross < $cost))", $data);
+            \enrol_liqpay\util::message_liqpay_error_to_admin(
+                get_string('paidnotenough', 'enrol_liqpay', "($data->payment_gross < $cost)"), 
+                $data);
             //die;
             redirect($CFG->wwwroot, get_string('paidnotenough', 'enrol_liqpay', "($data->payment_gross < $cost)"));
         }
